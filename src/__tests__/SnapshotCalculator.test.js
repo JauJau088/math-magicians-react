150b159e33c14532e2a55f02bb08a53e60d5a24b
tahenyBELGUITH <taheny.belguith@gmail.com>
2022-06-02 10:40:55 +0100

    Add snapshot test for calculator page

---
+++ src/__tests__/SnapshotCalculator.test.js
@@ -0,0 +1,10 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import Calculator from "../components/Calculator";
+
+describe("Snapshot test for Calculator page", () => {
+  it("It should render an identical template", () => {
+    const CalculatorPage = renderer.create(<Calculator />).toJSON();
+    expect(CalculatorPage).toMatchSnapshot();
+  });
+});
