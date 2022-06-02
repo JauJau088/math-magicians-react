fb2739eccadc28fb09107c7e469532ee237fa946
jaujau088 <jaujau088@gmail.com>
2022-06-02 11:53:32 +0200

    Add snapshot test for Quotes page

--- src/__tests__/SnapshotCalculator.test.js
+++ src/__tests__/SnapshotCalculator.test.js
@@ -1,9 +1,9 @@
-import React from "react";
-import renderer from "react-test-renderer";
-import Calculator from "../components/Calculator";
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Calculator from '../components/Calculator';
 
-describe("Snapshot test for Calculator page", () => {
-  it("It should render an identical template", () => {
+describe('Snapshot test for Calculator page', () => {
+  it('It should render an identical template', () => {
     const CalculatorPage = renderer.create(<Calculator />).toJSON();
     expect(CalculatorPage).toMatchSnapshot();
   });
